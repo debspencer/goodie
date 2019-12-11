04d00961596fe88fc75f9f4df496df6d8c73eebc
Deborah Spencer <debspencer@gmail.com>
2019-12-11 15:38:10 -0500

    remove default_css.go from .gitignore

---
+++ default_css.go
@@ -0,0 +1,45 @@
+package goodie
+var default_css = `
+.goodiebody {
+    background-color: white;
+}
+.goodieheader {
+    background: aqua;
+}
+.goodiefooter {
+    background: aqua;
+}
+.goodieerror {
+    color: red;
+}
+
+table.goodietable {
+
+}
+
+table.goodietable th {
+    background-color: #AAAAFF;
+}
+
+table.goodietable td {
+    background-color: aqua;
+}
+
+table.goodietable th:nth-child(odd) {
+    background-color: #AAAAFF;
+}
+
+table.goodietable td:nth-child(odd) {
+    background-color: aqua;
+}
+
+table.goodietable th:nth-child(even) {
+    background-color: #00FF00;
+}
+
+table.goodietable td:nth-child(even) {
+    background-color: aquamarine;
+}
+
+
+`
